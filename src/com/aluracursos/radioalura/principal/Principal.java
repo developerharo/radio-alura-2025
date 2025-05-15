02d1c768d80bb35215fe86939244ed43190b766d
developerharo <developerharo@gmail.com>
2025-05-14 21:56:38 -0600

    Creando metodos reutilizables, archivo MisFavoritos.java creado

--- src/com/aluracursos/radioalura/principal/Principal.java
+++ src/com/aluracursos/radioalura/principal/Principal.java
@@ -1,6 +1,7 @@
 package com.aluracursos.radioalura.principal;
 
 import com.aluracursos.radioalura.modelos.Cancion;
+import com.aluracursos.radioalura.modelos.MisFavoritos;
 import com.aluracursos.radioalura.modelos.Podcast;
 
 public class Principal {
@@ -13,16 +14,30 @@ public class Principal {
         miPodcast.setPresentador("Martha y Yordi");
         miPodcast.setTitulo("De todo un mucho");
 
+        // cancion
         for (int i = 0; i < 100; i++) {
             miCancion.meGusta();
         }
 
-        for (int i = 0; i < 5000; i++) {
+        for (int i = 0; i < 2000; i++) {
             miCancion.reproduce();
+        }
+
+        // podcast
+        for (int i = 0; i < 100; i++) {
+            miPodcast.meGusta();
+        }
+
+        for (int i = 0; i < 8000; i++) {
+            miPodcast.reproduce();
         }
 
         System.out.println("Total de reproducciones: " + miCancion.getTotalDeReproducciones());
         System.out.println("Total de Me Gusta:  " + miCancion.getTotalDeMeGusta());
+
+        MisFavoritos favoritos = new MisFavoritos();
+        favoritos.adicione(miPodcast);
+        favoritos.adicione(miCancion);
     }
 
 }
